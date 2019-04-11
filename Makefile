e4389da4df684a5da5920ec721adc521b626e91c
Alexander Foremny <aforemny@posteo.de>
2019-04-11 15:49:46 +0200

    Add material-components-elm.js

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
-build:
+build: node_modules
 	webpack --mode=development
+	elm make demo/Main.elm --output demo.js
+
+release: distclean node_modules
+	mkdir -p dist
+	webpack --mode=production
 	elm make --optimize demo/Main.elm --output demo.js
+	cp node_modules/material-components-web/dist/material-components-web.min.css dist/material-components-elm.min.css
+
+node_modules:
+	npm install
+
+clean:
+	rm -rf dist
+
+distclean: clean
+	rm -rf elm-stuff node_modules
--- webpack.config.js
+++ webpack.config.js
@@ -1,340 +1,47 @@
 const path = require("path");
 
-module.exports = [
-  {
-    entry: './src/button.js',
+module.exports =
+  [
+    "button",
+    "card",
+    "checkbox",
+    "chip",
+    "chip-set",
+    "dialog",
+    "drawer",
+    "enhanced-select",
+    "fab",
+    "form-field",
+    "helper-text",
+    "icon",
+    "icon-button",
+    "image-list",
+    "layout-grid",
+    "linear-progress",
+    "list",
+    "list-item",
+    "material-components-elm",
+    "menu",
+    "radio",
+    "ripple",
+    "select",
+    "slider",
+    "snackbar",
+    "switch",
+    "tab-bar",
+    "text-field",
+    "top-app-bar",
+  ].map(name => ({
+    entry: `./src/${name}.js`,
     output: {
       path: path.resolve(__dirname, "dist"),
-      filename: "mdc-button.min.js"
+      filename: name === "material-components-elm"
+        ? `${name}.min.js`
+        : `mdc-${name}.min.js`
     },
     module: {
       rules: [
         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
       ]
     }
-  },
-  {
-    entry: './src/card.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-card.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/layout-grid.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-layout-grid.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/checkbox.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-checkbox.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/chip-set.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-chip-set.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/chip.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-chip.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/dialog.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-dialog.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/drawer.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-drawer.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/fab.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-fab.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/form-field.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-form-field.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/list.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-list.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/list-item.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-list-item.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/icon.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-icon.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/icon-button.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-icon-button.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/image-list.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-image-list.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/linear-progress.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-linear-progress.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/menu.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-menu.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/radio.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-radio.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/ripple.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-ripple.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/select.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-select.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/enhanced-select.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-enhanced-select.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/slider.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-slider.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/snackbar.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-snackbar.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/switch.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-switch.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/tab-bar.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-tab-bar.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/text-field.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-text-field.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/helper-text.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-helper-text.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  },
-  {
-    entry: './src/top-app-bar.js',
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "mdc-top-app-bar.min.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-      ]
-    }
-  }
-];
+  }));

0696cadf949e9ae998acdff8b4490ee5a0a98025
Alexander Foremny <aforemny@posteo.de>
2019-08-05 11:38:54 +0200

    Document Material.TopAppBar

--- src/Material/TopAppBar.elm
+++ src/Material/TopAppBar.elm
@@ -1,29 +1,123 @@
 module Material.TopAppBar exposing
-    ( TopAppBarConfig, topAppBarConfig
-    , topAppBar, prominentTopAppBar, shortTopAppBar, shortCollapsedTopAppBar
-    , section, row
-    , alignEnd, alignStart
-    , title
-    , navigationIcon
+    ( topAppBar, topAppBarConfig, TopAppBarConfig
+    , row, section, alignEnd, alignStart
+    , navigationIcon, title
     , actionItem
     , fixedAdjust
     , denseFixedAdjust
     , denseProminentFixedAdjust
     , prominentFixedAdjust
     , shortFixedAdjust
+    , shortTopAppBar
+    , shortCollapsedTopAppBar
+    , prominentTopAppBar
     )
 
-{-|
+{-| Top App Bar acts as a container for items such as application title,
+navigation icon, and action items.
 
-@docs TopAppBarConfig, topAppBarConfig
-@docs topAppBar, prominentTopAppBar, shortTopAppBar, shortCollapsedTopAppBar
 
-@docs section, row
-@docs alignEnd, alignStart
+# Table of Contents
 
-@docs title
-@docs navigationIcon
+  - [Resources](#resources)
+  - [Basic Usage](#basic-usage)
+  - [Top App Bar](#top-app-bar)
+      - [Top App Bar with Action Items](#top-app-bar-with-action-items)
+  - [Fixed Variant](#fixed-variant)
+  - [Short Variant](#short-variant)
+      - [Short Always Closed Variant](#short-always-closed-variant)
+  - [Prominent Variant](#prominent-variant)
+  - [Dense Variant](#dense-variant)
+
+
+# Resources
+
+  - [Demo: Top App Bars](https://aforemny.github.io/material-components-elm/#top-app-bars)
+  - [Material Design Guidelines: Top App Bar](https://material.io/go/design-app-bar-top)
+  - [MDC Web: List](https://github.com/material-components/material-components-web/tree/master/packages/mdc-top-app-bar)
+  - [Sass Mixins (MDC Web)](https://github.com/material-components/material-components-web/tree/master/packages/mdc-top-app-bar#sass-mixins)
+
+
+# Basic Usage
+
+    import Material.TopAppBar as TopAppBar
+        exposing
+            ( topAppBar
+            , topAppBarConfig
+            )
+
+    main =
+        topAppBar topAppBarConfig
+            [ TopAppBar.row []
+                [ TopAppBar.section [ TopAppBar.alignStart ]
+                    [ iconButton
+                        { iconButtonConfig
+                            | additionalAttributes =
+                                [ TopAppBar.navigationIcon ]
+                        }
+                        "menu"
+                    , Html.span [ TopAppBar.title ]
+                        [ text "Title" ]
+                    ]
+                ]
+            ]
+
+
+# Top App Bar
+
+Usually a top app bar contains one row with at least one start-aligned section.
+This is where you would normally place your navigation icon and title.
+
+@docs topAppBar, topAppBarConfig, TopAppBarConfig
+@docs row, section, alignEnd, alignStart
+@docs navigationIcon, title
+
+
+## Top App Bar with Action Items
+
+A top app bar can contain action items that are placed on the opposite side of
+the navigation icon. To do so, add another end-aligned section to the top app
+bar's row. Do not forget to set the `actionItem` attribute on the icons.
+
+    topAppBar topAppBarConfig
+        [ TopAppBar.row []
+            [ TopAppBar.section [ TopAppBar.alignStart ]
+                [ iconButton
+                    { iconButtonConfig
+                        | additionalAttributes =
+                            [ TopAppBar.navigationIcon ]
+                    }
+                    "menu"
+                , Html.span [ TopAppBar.title ]
+                    [ text "Title" ]
+                ]
+            , TopAppBar.section [ TopAppBar.alignEnd ]
+                [ iconButton
+                    { iconButtonConfig
+                        | additionalAttributes =
+                            [ TopAppBar.actionItem ]
+                    }
+                    "print"
+                , iconButton
+                    { iconButtonConfig
+                        | additionalAttributes =
+                            [ TopAppBar.actionItem ]
+                    }
+                    "bookmark"
+                ]
+            ]
+        ]
+
 @docs actionItem
+
+
+# Fixed Variant
+
+To make a top app bar fixed to the top, set its `fixed` configuration field to
+`True`. Since a fixed top app bar would overlay the pages content, an
+appropriate margin has to be applied to the page's content.
+
+    topAppBar { topAppBarConfig | fixed = True } []
 
 @docs fixedAdjust
 @docs denseFixedAdjust
@@ -31,13 +125,48 @@ module Material.TopAppBar exposing
 @docs prominentFixedAdjust
 @docs shortFixedAdjust
 
+
+# Short Variant
+
+Short top app bars collapse to the navigation icon side when scrolled.
+
+    shortTopAppBar topAppBarConfig []
+
+@docs shortTopAppBar
+
+
+## Short Always Closed Variant
+
+A short top app bar can be configured to always appear closed.
+
+    shortCollapsedtopAppBar topAppBarConfig []
+
+@docs shortCollapsedTopAppBar
+
+
+# Prominent Variant
+
+To make a top app bar taller than the default, you may use a prominent top app bar.
+
+    prominentTopAppBar topAppBarConfig []
+
+@docs prominentTopAppBar
+
+
+# Dense Variant
+
+To make a top app bar shorter than the default, set its `dense` configuration
+field to `True`.
+
+    topAppBar { topAppBarConfig | dense = True }
+
 -}
 
 import Html exposing (Html, text)
 import Html.Attributes exposing (class)
 
 
-{-| TODO docs
+{-| Configuration of a top app bar
 -}
 type alias TopAppBarConfig msg =
     { variant : Variant
@@ -54,7 +183,7 @@ type Variant
     | Prominent
 
 
-{-| TODO docs
+{-| Default configuration of a top app bar
 -}
 topAppBarConfig : TopAppBarConfig msg
 topAppBarConfig =
@@ -65,7 +194,7 @@ topAppBarConfig =
     }
 
 
-{-| TODO docs
+{-| Top app bar view function
 -}
 topAppBar : TopAppBarConfig msg -> List (Html msg) -> Html msg
 topAppBar config nodes =
@@ -81,70 +210,75 @@ topAppBar config nodes =
         nodes
 
 
-{-| TODO docs
+{-| Short top app bar view function
 -}
 shortTopAppBar : TopAppBarConfig msg -> List (Html msg) -> Html msg
 shortTopAppBar config nodes =
     topAppBar { config | variant = Short } nodes
 
 
-{-| TODO docs
+{-| Short always closed top app bar view function
 -}
 shortCollapsedTopAppBar : TopAppBarConfig msg -> List (Html msg) -> Html msg
 shortCollapsedTopAppBar config nodes =
     topAppBar { config | variant = ShortCollapsed } nodes
 
 
-{-| TODO docs
+{-| Prominent top app bar view function
 -}
 prominentTopAppBar : TopAppBarConfig msg -> List (Html msg) -> Html msg
 prominentTopAppBar config nodes =
     topAppBar { config | variant = Prominent } nodes
 
 
-{-| TODO docs
+{-| A row is the first child of a top app bar. It contains the top app bar's
+`section`s.
 -}
 row : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 row attributes nodes =
     Html.section ([ class "mdc-top-app-bar__row" ] ++ attributes) nodes
 
 
-{-| TODO docs
+{-| Sections subdivide the top app bar's rows. A section may be start- or
+end-aligned. Usually, the first section is start-aligned and contains the top
+app bar's navigation icon and title.
 -}
 section : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 section attributes nodes =
     Html.section ([ class "mdc-top-app-bar__section" ] ++ attributes) nodes
 
 
-{-| TODO docs
+{-| Start-align a top app bar's `section`
 -}
 alignStart : Html.Attribute msg
 alignStart =
     class "mdc-top-app-bar__section--align-start"
 
 
-{-| TODO docs
+{-| End-align a top app bar's `section`
 -}
 alignEnd : Html.Attribute msg
 alignEnd =
     class "mdc-top-app-bar__section--align-end"
 
 
-{-| TODO docs
+{-| Apply this attribute to an icon button to mark it as a top app bar's
+navigation icon
 -}
 navigationIcon : Html.Attribute msg
 navigationIcon =
     class "mdc-top-app-bar__navigation-icon"
 
 
-{-| TODO docs
+{-| Apply this attribute to a element to mark it as the top app bar's title
 -}
 title : Html.Attribute msg
 title =
     class "mdc-top-app-bar__title"
 
 
-{-| TODO docs
+{-| Apply this attribute to a icon button to mark it as a top app bar's action
+item
 -}
 actionItem : Html.Attribute msg
 actionItem =
@@ -190,35 +324,55 @@ fixedCs { fixed } =
         Nothing
 
 
-{-| TODO docs
+{-| Appropriate padding for a fixed top app bar.
+
+Apply this to the page's content so that a fixed top app bar does not overlay
+the content.
+
 -}
 fixedAdjust : Html.Attribute msg
 fixedAdjust =
     class "mdc-top-app-bar--fixed-adjust"
 
 
-{-| TODO docs
+{-| Appropriate padding for a dense fixed top app bar.
+
+Apply this to the page's content so that a fixed top app bar does not overlay
+the content.
+
 -}
 denseFixedAdjust : Html.Attribute msg
 denseFixedAdjust =
     class "mdc-top-app-bar--dense-fixed-adjust"
 
 
-{-| TODO docs
+{-| Appropriate padding for a short fixed top app bar.
+
+Apply this to the page's content so that a fixed top app bar does not overlay
+the content.
+
 -}
 shortFixedAdjust : Html.Attribute msg
 shortFixedAdjust =
     class "mdc-top-app-bar--short-fixed-adjust"
 
 
-{-| TODO docs
+{-| Appropriate padding for a prominent fixed top app bar.
+
+Apply this to the page's content so that a fixed top app bar does not overlay
+the content.
+
 -}
 prominentFixedAdjust : Html.Attribute msg
 prominentFixedAdjust =
     class "mdc-top-app-bar--prominent-fixed-adjust"
 
 
-{-| TODO docs
+{-| Appropriate padding for a dense prominent fixed top app bar.
+
+Apply this to the page's content so that a fixed top app bar does not overlay
+the content.
+
 -}
 denseProminentFixedAdjust : Html.Attribute msg
 denseProminentFixedAdjust =

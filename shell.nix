1dadaf16a59828a6554656906c8b7cb1fed5729f
Alexander Foremny <aforemny@posteo.de>
2020-02-17 09:09:21 +0100

    chore: Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-with stdenv;
-mkDerivation {
-  name = "material-components";
+mkShell {
   buildInputs = [ elmPackages.elm nodejs ];
   shellHook = ''
     export PATH=./node_modules/.bin:$PATH

3de9303875658f83c475f9ab6d75b01556682cb9
Alexander Foremny <aforemny@posteo.de>
2020-02-29 14:46:13 +0100

    chore: Add bin/check-links.sh

---
+++ bin/check-links.sh
@@ -0,0 +1,50 @@
+fetch=$(mktemp fetch.XXXXXX.js)
+
+cat > "$fetch" <<EOF
+const connect = require("connect");
+const serveStatic = require("serve-static");
+const puppeteer = require("puppeteer-core");
+const http = require("http");
+
+(async () => {
+
+  const root = process.argv[2];
+  const urls = process.argv.slice(3);
+  const app = connect().use(serveStatic("./gh-pages"));
+  const server = http.createServer(app);
+  await new Promise((resolve, reject) => server.listen(8080, () => resolve()));
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: process.env.CHROMIUM_PATH
+  });
+  const page = await browser.newPage();
+  let errors = [];
+
+  for (url of urls) {
+    await page.goto(url);
+    if (await page.\$eval("body", node => !!node.innerText.match(/404/))) {
+      errors.push(url);
+    }
+  }
+
+  for (error of errors) {
+    console.log(error);
+  }
+
+  await browser.close();
+  await new Promise((resolve, reject) => server.close(() => resolve()));
+
+  process.exit(errors.length === 0 ? 0 : 1);
+})();
+EOF
+
+find src -name "*.elm" \
+  | xargs grep -h -o -E '\((https://aforemny.github.io/material-components-web-elm.*)\)' \
+  | sed 's/[()]//g' \
+  | sort \
+  | uniq \
+  | sed 's@.*#@http://localhost:8080/#@' \
+  | xargs node "$fetch" ./gh-pages
+
+rm -f "$fetch"
--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ elmPackages.elm nodejs ];
+  buildInputs = [ elmPackages.elm nodejs chromium ];
   shellHook = ''
     export PATH=./node_modules/.bin:$PATH
+    export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+    export CHROMIUM_PATH=${chromium}/bin/chromium
+    export PUPPETEER_PRODUCT=firefox
   '';
 }

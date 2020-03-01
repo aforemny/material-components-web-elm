cfb2217c16c98787217a3eb2a638759b606635d6
Alexander Foremny <aforemny@posteo.de>
2020-03-01 10:11:03 +0100

    chore: Document utils/className.js

--- src/utils/className.js
+++ src/utils/className.js
@@ -1,3 +1,31 @@
+/* Material Components frequently enhance their DOM elements with additional
+ * class names that depend on component state (ie. has focus) rather than
+ * configuration state (ie. text field value).
+ *
+ * Since the component state is unknown to Elm, we cannot control those class
+ * names in Elm. On the other hand, Elm does controls some class names (ie.
+ * mdc-text-field).
+ *
+ * This means that currently, Elm's virtual dom library assumes sole control
+ * over class names, since it sets a DOM node's `className` property. Thus, if
+ * a Elm-controlled class name changes, it might delete additional uncontrolled
+ * class names.
+ *
+ * We work around this issue by patching a DOM node's `className` property in
+ * the following way:
+ * (1) We may assume that Elm only ever touches `className`
+ * (2) We may assume that Material Components only ever touch `classList`
+ *
+ * Thus, whenever a write to `className` happens, we record the list of classes
+ * that Elm has control over (classes that have been added via `className`).
+ * Then we identify classes removed via `className`, and we only allow removal
+ * of classes that Elm has control over. Classes added via the `classList`
+ * property cannot be removed via the `className` property.
+ *
+ * We will not require this fix anymore once Elm's virtual dom implementation
+ * uses `classList.add`, `classList.remove` to manage class names.
+ */
+
 function setClassName(className) {
   const css = className.split(" ").filter(cs => cs !== "");
   for (let cs of css) {

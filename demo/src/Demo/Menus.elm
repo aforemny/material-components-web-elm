a15e48e4e9479b66269e298315c50847c355d8eb
Alexander Foremny <aforemny@posteo.de>
2021-01-15 16:07:03 +0100

    doc: Add card example to menu demo

--- demo/src/Demo/Menus.elm
+++ demo/src/Demo/Menus.elm
@@ -4,17 +4,20 @@ import Demo.CatalogPage exposing (CatalogPage)
 import Html exposing (Html, text)
 import Html.Attributes exposing (class, style)
 import Material.Button as Button
+import Material.Card as Card
 import Material.IconButton as IconButton
 import Material.List as List
 import Material.List.Divider as ListDivider
 import Material.List.Item as ListItem exposing (ListItem)
 import Material.Menu as Menu
+import Material.Theme as Theme
 import Material.Typography as Typography
 
 
 type alias Model =
     { open1 : Bool
     , open2 : Bool
+    , open3 : Bool
     }
 
 
@@ -22,6 +25,7 @@ defaultModel : Model
 defaultModel =
     { open1 = False
     , open2 = False
+    , open3 = False
     }
 
 
@@ -29,6 +33,7 @@ type Msg
     = Open
     | Close
     | OpenChanged Bool
+    | Open3Changed Bool
 
 
 update : Msg -> Model -> Model
@@ -42,6 +47,9 @@ update msg model =
 
         OpenChanged open2 ->
             { model | open2 = open2 }
+
+        Open3Changed open3 ->
+            { model | open3 = open3 }
 
 
 view : Model -> CatalogPage Msg
@@ -59,6 +67,8 @@ view model =
         , buttonExample model
         , Html.h3 [ Typography.subtitle1 ] [ text "Icon Button Menu" ]
         , iconButtonExample model
+        , Html.h3 [ Typography.subtitle1 ] [ text "Icon Button Menu within Card" ]
+        , iconButtonWithinCardExample model
         ]
     }
 
@@ -97,6 +107,75 @@ iconButtonExample model =
                 )
         )
         (IconButton.icon "menu_vert")
+
+
+iconButtonWithinCardExample : Model -> Html Msg
+iconButtonWithinCardExample model =
+    Card.card (Card.config |> Card.setAttributes [ style "width" "350px" ])
+        { blocks =
+            Card.primaryAction []
+                [ Card.block <|
+                    Html.div
+                        [ style "padding" "1rem" ]
+                        [ Html.h2
+                            [ Typography.headline6
+                            , style "margin" "0"
+                            ]
+                            [ text "Our Changing Planet" ]
+                        , Html.h3
+                            [ Typography.subtitle2
+                            , Theme.textSecondaryOnBackground
+                            , style "margin" "0"
+                            ]
+                            [ text "by Kurt Wagner" ]
+                        ]
+                , Card.block <|
+                    Html.div
+                        [ Typography.body2
+                        , Theme.textSecondaryOnBackground
+                        , style "padding" "0 1rem 0.5rem 1rem"
+                        ]
+                        [ text
+                            """
+                Visit ten places on our planet that are undergoing the biggest
+                changes today.
+                """
+                        ]
+                ]
+        , actions =
+            Just <|
+                Card.actions
+                    { buttons =
+                        [ Card.button Button.config "Read"
+                        , Card.button Button.config "Bookmark"
+                        ]
+                    , icons =
+                        [ Card.icon IconButton.config
+                            (IconButton.icon "favorite_border")
+                        , Card.icon IconButton.config
+                            (IconButton.icon "share")
+                        , Card.icon
+                            (IconButton.config
+                                |> IconButton.setOnClick (OpenChanged True)
+                                |> IconButton.setMenu
+                                    (Just <|
+                                        IconButton.menu
+                                            (Menu.config
+                                                |> Menu.setOpen model.open3
+                                                |> Menu.setOnClose
+                                                    (OpenChanged False)
+                                            )
+                                            (menuItems
+                                                { onClick =
+                                                    OpenChanged False
+                                                }
+                                            )
+                                    )
+                            )
+                            (IconButton.icon "more_vert")
+                        ]
+                    }
+        }
 
 
 menuItems : { onClick : msg } -> List (Html msg)

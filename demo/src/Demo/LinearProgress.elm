5e59f2ff37d6a1024c6ac3c9ab2e0a1b1e19b107
Alexander Foremny <aforemny@posteo.de>
2021-09-10 12:07:33 +0200

    feat!: Remove LinearProgress.setReverse
    BREAKING CHANGE: The function `LinearProgress.setReverse` has been
    removed as the direction of text is now automatically determined.
    
    Refer to the documentation for ways to manually set the direction in
    case this fails or is not desired.

--- demo/src/Demo/LinearProgress.elm
+++ demo/src/Demo/LinearProgress.elm
@@ -2,6 +2,7 @@ module Demo.LinearProgress exposing (Model, Msg, defaultModel, update, view)
 
 import Demo.CatalogPage exposing (CatalogPage)
 import Html exposing (text)
+import Html.Attributes
 import Material.LinearProgress as LinearProgress
 import Material.Typography as Typography
 
@@ -33,18 +34,36 @@ view model =
         , documentation = Just "https://package.elm-lang.org/packages/aforemny/material-components-web-elm/latest/Material-LinearProgress"
         , sourceCode = Just "https://github.com/material-components/material-components-web/tree/master/packages/mdc-linear-progress"
         }
-    , hero = [ LinearProgress.determinate LinearProgress.config { progress = 0.5 } ]
+    , hero =
+        [ LinearProgress.determinate LinearProgress.config { progress = 0.5 } ]
     , content =
-        [ Html.h3 [ Typography.subtitle1 ] [ text "Buffered" ]
-        , LinearProgress.buffered LinearProgress.config { progress = 0.5, buffered = 0.75 }
-        , Html.h3 [ Typography.subtitle1 ] [ text "Indeterminate" ]
-        , LinearProgress.indeterminate LinearProgress.config
-        , Html.h3 [ Typography.subtitle1 ] [ text "Reversed" ]
-        , LinearProgress.determinate
-            (LinearProgress.config |> LinearProgress.setReverse True)
-            { progress = 0.5 }
-        , Html.h3 [ Typography.subtitle1 ] [ text "Reversed Buffered" ]
-        , LinearProgress.buffered (LinearProgress.config |> LinearProgress.setReverse True)
+        [ Html.h3 [ Typography.subtitle1 ] [ text "Determinate" ]
+        , LinearProgress.determinate LinearProgress.config { progress = 0.5 }
+        , Html.h3 [ Typography.subtitle1 ] [ text "Buffered" ]
+        , LinearProgress.buffered LinearProgress.config
             { progress = 0.5, buffered = 0.75 }
+        , Html.h3 [ Typography.subtitle1 ] [ text "Indeterminate" ]
+        , LinearProgress.indeterminate
+            (LinearProgress.config
+                |> LinearProgress.setAttributes [ Html.Attributes.dir "rtl" ]
+            )
+        , Html.h3 [ Typography.subtitle1 ] [ text "Right to Left Localized" ]
+        , Html.h4 [ Typography.subtitle1 ] [ text "Determinate" ]
+        , LinearProgress.determinate
+            (LinearProgress.config
+                |> LinearProgress.setAttributes [ Html.Attributes.dir "rtl" ]
+            )
+            { progress = 0.5 }
+        , Html.h4 [ Typography.subtitle1 ] [ text "Buffered" ]
+        , LinearProgress.buffered
+            (LinearProgress.config
+                |> LinearProgress.setAttributes [ Html.Attributes.dir "rtl" ]
+            )
+            { progress = 0.5, buffered = 0.75 }
+        , Html.h4 [ Typography.subtitle1 ] [ text "Indeterminate" ]
+        , LinearProgress.indeterminate
+            (LinearProgress.config
+                |> LinearProgress.setAttributes [ Html.Attributes.dir "rtl" ]
+            )
         ]
     }
